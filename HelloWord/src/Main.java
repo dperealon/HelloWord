b8c08e816259fde122189dd1f54ebf5c63585df8
Administrador <Administrador@10.0.2.15>
2018-05-22 21:30:29 +0200

    Prueba de commit

---
+++ HelloWord/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		int rand;
+		for (int i = 0; i <= 10; i++) {
+			rand = numeroRandom(0, 100);
+			if(numeros.indexOf(rand) == -1) {
+				numeros.add(rand);
+			}else {
+				i--;
+			}
+		}
+		
+		for (Integer numero : numeros) {
+			System.out.println(numero);
+		}
+		
+	}
+	
+	public static int numeroRandom(int minimo, int maximo) {
+		
+		int rand = (int) (Math.random() * (maximo+1)) + minimo; 
+		
+		return rand;
+		
+	}
+	
+}
