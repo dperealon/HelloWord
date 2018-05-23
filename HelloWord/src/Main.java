71bc5241049ad022f5113247f9e3c2c3afe99fcc
Administrador <Administrador@10.0.2.15>
2018-05-23 21:22:40 +0200

    Cambio realizado en el main

--- HelloWord/src/Main.java
+++ HelloWord/src/Main.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo!");
+		
 		ArrayList<Integer> numeros = new ArrayList<Integer>();
 		int rand;
 		for (int i = 0; i <= 10; i++) {
